335d4e0d111e950af2c7908bae18e6fd060df32b
tahuuhao <hao.th@zinza.com.vn>
2023-04-11 14:04:48 +0700

    a

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,10 +1,17 @@
 import styles from './index.module.css';
+import { vi } from 'date-fns/locale';
+import { format, parseISO } from 'date-fns';
+
+export const formatDateVN = (date: string) => {
+  return format(parseISO(date), 'dd/MM/yyyy');
+};
 
 export function Index(props: { date: string }) {
   return (
     <div className={styles.page}>
       <div className="wrapper">
         <div className="container">
+          {formatDateVN('2023-04-11T02:20:51.332Z')}
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
