39ab173cc43434099d07ec5d0d280140d94fccf7
tahuuhao <hao.th@zinza.com.vn>
2023-04-11 11:20:23 +0700

    create bug

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,6 +1,10 @@
 import styles from './index.module.css';
 import { vi } from 'date-fns/locale';
-import { format } from 'date-fns';
+import { format, parseISO} from 'date-fns';
+
+export const formatDateVN = (date: string) => {
+  return format(parseISO(date), 'dd/MM/yyyy');
+};
 
 export function Index() {
   /*
@@ -12,7 +16,14 @@ export function Index() {
     <div className={styles.page}>
       <div className="wrapper">
         <div className="container">
-          {format(new Date(), 'do MMMM', { locale: vi })}
+          <p>{format(new Date(), 'do MMMM', { locale: vi })}</p>
+          {
+            new Date().toLocaleDateString()
+          }
+          <br/>
+          {
+            formatDateVN('2023-04-11T02:20:51.332Z')
+          }
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
