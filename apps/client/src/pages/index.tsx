0807a2b45ad539a21afca0dbcbd3e6042807eace
tahuuhao <hao.th@zinza.com.vn>
2023-04-12 10:06:24 +0700

    fix

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,15 +1,10 @@
 import styles from './index.module.css';
-import {formatDateTimeVN, formatDateVN, greetingMessage} from '@client/utils/date'
+import {formatDateTimeVN, formatDateVN, formatDateVNLocalTimeStamp, greetingMessage} from '@client/utils/date'
 import { useEffect, useState } from 'react';
 import { GreetingMessage } from '@client/components/GreetingMessage';
 import { TimeRender } from '@client/components/TimeRender';
 
 export function Index(props: { date: string }) {
-  const [hourse, setHourse] = useState(0)
-  useEffect(()=>{
-    const date = new Date()
-    setHourse(date.getHours())
-  },[])
   return (
     <div className={styles.page}>
       <div className="wrapper">
@@ -18,9 +13,9 @@ export function Index(props: { date: string }) {
           <p>
             <GreetingMessage/>
           </p>
-          {/* <p>
-            {formatDateTimeVN('2023-04-11T03:10:29.208Z')}
-          </p> */}
+          <p>
+            {formatDateVNLocalTimeStamp(new Date('2023-04-11T03:10:29.208Z'))}
+          </p>
           <TimeRender time='2023-04-12T02:32:54.697Z'/>
           {/* <p>
             {formatDateTimeVN('2023-04-12T02:32:54.697Z')}
